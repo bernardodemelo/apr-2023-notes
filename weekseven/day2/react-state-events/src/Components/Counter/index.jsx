b25cf807846abf770cecb74ce3c8709a7e381a44
bernardomelo-ironhack-WDLT <bernardo.melo@ironhack.com>
2023-05-23 10:16:21 +0100

    day2_afterbreak

---
+++ weekseven/day2/react-state-events/src/Components/Counter/index.jsx
@@ -0,0 +1,16 @@
+// import useState React Hook from React
+import {useState} from 'react';
+
+function Counter(){
+    const [count, setCount] = useState(0);
+
+    return(<div className="counter">
+     <h2>Counter</h2>
+        <p>You clicked {count} times!</p>
+        <button onClick={()=> setCount(count-1)}> - </button>
+        <button onClick={()=> setCount(count+1)}> + </button>
+    </div>)
+
+}
+
+export default Counter;
