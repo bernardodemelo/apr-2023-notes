4bd30a5eb83478870330d2813969780bed0e79b1
bernardomelo-ironhack-WDLT <bernardo.melo@ironhack.com>
2023-05-29 12:43:13 +0100

    project-server

--- weekeight/day1/project-management-server/app.js
+++ weekeight/day1/project-management-server/app.js
@@ -24,6 +24,9 @@ app.use("/auth", authRoutes);
 const projectRoutes = require('./routes/project.routes');
 app.use('/api', projectRoutes);
 
+const taskRoutes = require('./routes/task.routes');
+app.use('/api', taskRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
